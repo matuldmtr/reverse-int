a8c0e20b2776f6d50325de8a8b33778bc2fcd717
Dmitry Matul <matuldmtr@gmail.com>
2023-08-02 20:55:07 +0200

    :docs: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n > 0
+        ? +n.toString().split("").reverse().join("")
+        : +n.toString().split("").reverse().slice(0, -1).join("");
+};
